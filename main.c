1e749ab457f0a0b460e8274ba1ebcada83d388c2
Bitkd76 <bilal.elayoubi7>
2025-11-30 20:59:19 +0100

    reformulation + commentaire (main.c)

--- main.c
+++ main.c
@@ -15,11 +15,12 @@ int main(){                                         // Menu principal [Bilal]
     printf("3 - affichage de tous les trajtes tries par ville de depart et date de depart \n\n");
     printf("4 - ajout et suppression de clients a partir du numero d'un bus\n\n");
     printf("5 - ajouter/modifier le nom/prix billet d'un client selon num bus + iDclient  \n\n");
-    printf("6 - la sauvegarde des donnees dans un fichierhier \n\n");
-    printf("7 - un filtre sur les trajets partant d'une ville, partant a une date, arrivant le lendemain \n\n");
-    printf("8 - un filtre sur les trajets combinant ville de depart, ville d’arrivee et date de depart \n\n");
-    printf("9 - l’affichage des trajets tries par chiffre d’affaires (difference entre les prix des billets payes par les passagers et le cout du trajet supporte par l’entreprise), avec affichage de ce chiffre d’affaires \n\n");
-    printf("10 - La possibilite de supprimer tous les bus avec un chiffre d’affaires negatif, en repartissant les passagers sur les trajets identiques, en priorisant les passagers ayant paye le plus sur les dates et horaires les plus proches du trajet initial. Il faudra indiquer le gain genere par cette operation \n\n");
+    printf("6 - Sauvegarde des donnees dans un fichier \n\n");
+    printf("7 - Trajets partant d'une ville, partant a une date, arrivant le lendemain \n\n");
+    printf("8 - Trajets combinant ville de depart, ville arrivee et date de depart \n\n");
+    printf("9 - Trajets tries par CA \n\n"); //(difference entre les prix des billets payes par les passagers et le cout du trajet supporte par l’entreprise)//, 
+    // en dernier si on a fini //
+    printf("10 - Suppression des bus avec CA negatif, en repartissant les passagers sur les trajets identiques, en priorisant les passagers ayant paye le plus sur les dates et horaires les plus proches du trajet initial. Il faudra indiquer le gain genere par cette operation \n\n");
     printf("\n\n");
 
     FILE *fichier = fopen(trajet, "r") ;
